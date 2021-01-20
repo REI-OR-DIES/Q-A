c2cdebdd4fc9dd668844eed816646825b79ca8ee
Evan Glaser <glaser.evan@gmail.com>
2021-01-20 14:39:41 -0700

    rendering first question property values

--- client/components/App.jsx
+++ client/components/App.jsx
@@ -1,10 +1,37 @@
-import React, { Component } from 'react';
+import React, { useState, useEffect } from 'react';
 import ReactDOM from 'react-dom';
+import axios from 'axios';
+import Question from './Question';
 
-const App = () => (
-  <div>
-    <h1>My New React App</h1>
-  </div>
-);
+const App = () => {
+  const [questionList, setQuestionList] = useState([]);
+
+  const getQuestionList = () => {
+    axios.get('http://localhost:3007/api/questions')
+      .then(({ data }) => {
+        const firstQ = data[0];
+        console.log('firstQ is :', firstQ);
+        setQuestionList(firstQ);
+      });
+  };
+  useEffect(() => {
+    getQuestionList();
+  }, []);
+
+  return (
+    <div>
+      <h1>Questions and Answers</h1>
+      <button
+        type="submit"
+        className="askQuestion"
+      >
+        Ask a question
+      </button>
+      <Question
+        questionList={questionList}
+      />
+    </div>
+  );
+};
 
 export default App;
--- server/index.js
+++ server/index.js
@@ -8,6 +8,10 @@ const app = express();
 app.use(express.static(path.join(__dirname, '..', 'public')));
 app.use(bodyParser.json());
 app.use(bodyParser.urlencoded({ extended: true }));
+app.use((req, res, next) => {
+  res.header('Access-Control-Allow-Origin', '*');
+  next();
+});
 
 const { findAllQuestions, addNewQuestion, answerQuestion } = require('../database/db/index.js');
 
